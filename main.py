304af8e703d39e0d8d976cdfe30c4c1fc5f56595
Norbert Lewicz <lewy054@gmail.com>
2021-05-08 13:58:13 +0200

    add voxel class

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from voxel import Voxel
 
 app = Ursina()
+player = FirstPersonController()
+
+for z in range(16):
+    for x in range(16):
+        voxel = Voxel(position=(x, 0, z))
+
 app.run()
---
+++ voxel.py
@@ -0,0 +1,14 @@
+from ursina import *
+
+
+class Voxel(Button):
+    def __init__(self, position=(0, 0, 0)):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model='cube',
+            origin_y=0.5,
+            texture='white_cube',
+            color=color.white,
+            highlight_color=color.light_gray
+        )
